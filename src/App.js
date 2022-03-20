8449e8645eaf216961e13a1e65a06a6bf09f717b
unknown <ryancare@protonmail.com>
2022-03-20 10:42:52 -0500

    Added onClick even handler for likes icon

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData);
-  console.log(posts);
   // const [search, setSearch] = useState("");
   const likePost = postId => {
     setPosts(posts.map(val => {
+      console.log(val);
       if (val.id === postId) {
         val.likes += 1};
       return val } ) )
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,11 +5,10 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-console.log(posts);
   return (
     <div className='posts-container-wrapper'>
       
-      {Array.from(posts, p => <Post likePost={likePost} post={p} key={p.id.toString()} /> )  /* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(p => <Post likePost={likePost} post={p} key={p.id.toString()} /> )  /* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
